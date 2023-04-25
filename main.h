5a0a85f759543f6b754f2c014c545af0fab79969
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-26 00:51:45 +0200

    yarbi ghi hadi

--- main.h
+++ main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-
 int _printf(const char *format, ...);
 int _strlen(char *s);
-void print_c(va_list arg);
+int print_c(va_list arg);
 int print_s(va_list arg);
-int print_cs(const char *format, va_list ap);
+int print_switch(const char *format, va_list ap, int i);
 int print_di(va_list arg);
 #endif /* MAIN_H */
